6afcde635338c1d191c00b35676aa27c8ab34f48
victoria-huang <vctrah@gmail.com>
2018-04-10 17:00:26 -0400

    add form and edit model to associate song with artist/genre/notes

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,5 +2,33 @@ class Song < ActiveRecord::Base
   belongs_to :artist
   belongs_to :genre
   has_many :notes
-end
 
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by(name: name)
+  end
+
+  def artist_name
+    self.artist.name if self.artist
+  end
+
+  def genre_name=(name)
+    self.genre = Genre.find_by(name: name)
+  end
+
+  def genre_name
+    self.genre.name if self.genre
+  end
+
+  def note_contents=(contents)
+    contents.each do |content|
+      note = Note.find_or_create_by(content: content)
+      self.notes << note
+    end
+  end
+
+  def note_contents
+    if self.notes
+      self.notes.map { |note| note.content }.reject { |content| content.strip.empty? }
+    end
+  end
+end
